a38714787d4bd89a2e3efd7adcb7a5661b5c5f8a
natsutaka <natsu_summer_0@yahoo.co.jp>
2021-03-29 21:54:29 +0900

    部屋とキャラクターを結合しました

---
+++ Navigation/Assets/Programs/Expose2DScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Expose Scene Property/3D")]
+public class Expose2DScene : ScriptableObject
+{
+	public Transform player { get; set; }
+}
